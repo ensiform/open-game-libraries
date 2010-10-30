0aa7286dd459472a998fdd2d9751cbf59bfc87ab
Roughael <core@roughael.net>
2010-10-30 18:57:00 +0000

    added doxygen comments for LockFreeQueue, LowLockQueue and the Atomic operations. fixed problem with Atomic operations.. my assembler knowledge is a little rusted

--- Libraries/Include/og/Shared/Thread/LockFreeQueue.h
+++ Libraries/Include/og/Shared/Thread/LockFreeQueue.h
@@ -1,90 +1,133 @@
-/*
-===========================================================================
-The Open Game Libraries.
-Copyright (C) 2007-2010 Lusito Software
+// ==============================================================================
+//! @file
+//! @brief	Lock Free Queue and Low Lock Queue
+//! @author	Herb Sutter: http://www.drdobbs.com/high-performance-computing/210604448
+//! @author	Santo Pfingsten (TTK-Bandit)
+//! @note	Copyright (C) 2007-2010 Lusito Software
+// ==============================================================================
+//
+// The Open Game Libraries.
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+// ==============================================================================
 
-Author:  Herb Sutter: http://www.drdobbs.com/high-performance-computing/210604448
-Author:  Santo Pfingsten (TTK-Bandit)
-Purpose: Lock Free Queue
------------------------------------------
-
-This software is provided 'as-is', without any express or implied
-warranty. In no event will the authors be held liable for any damages
-arising from the use of this software.
-
-Permission is granted to anyone to use this software for any purpose,
-including commercial applications, and to alter it and redistribute it
-freely, subject to the following restrictions:
-
-1. The origin of this software must not be misrepresented; you must not
-   claim that you wrote the original software. If you use this software
-   in a product, an acknowledgment in the product documentation would be
-   appreciated but is not required.
-
-2. Altered source versions must be plainly marked as such, and must not be
-   misrepresented as being the original software.
-
-3. This notice may not be removed or altered from any source distribution.
-===========================================================================
-*/
 
 #ifndef __OG_LOCK_FREE_QUEUE_H__
 #define __OG_LOCK_FREE_QUEUE_H__
 
 #include <og/Shared/Thread/Thread.h>
 
+//! Open Game Libraries
 namespace og {
-	OG_INLINE void AtomicExchangePointer( void **target, void *value ) {
+//! @defgroup Shared Shared (Library)
+//! @{
+
+	// ==============================================================================
+	//! Atomic exchange pointer
+	//!
+	//! @param	target		The target
+	//! @param	exchange	The exchange address
+	//!
+	//! @return	The initial address pointed to by target
+	// ==============================================================================
+	OG_INLINE void *AtomicExchangePointer( void **target, void *exchange ) {
+		void *old;
 #if OG_ASM_MSVC
 		__asm {
-			mov eax, value;
+			mov eax, exchange;
 			mov edx, target;
 			lock xchg [edx], eax;
+			mov old, eax;
 		}
 #elif OG_ASM_GNU
-		void *old;
 		__asm__ __volatile__(
 			"lock xchg %0,%1"
-			: "=r" old, "=m" (*target)
-			:  "0" val
+			: "=r" (old), "=m" (*target)
+			:  "0" (exchange)
 		);
 #endif
 	}
-	OG_INLINE void *AtomicCompareExchangePointer( void **target, void *value, void *comparand ) {
-		void *old = value;
+
+	// ==============================================================================
+	//! Atomic compare exchange pointer
+	//!
+	//! @param	target		The target
+	//! @param	exchange	The exchange address
+	//! @param	comparand	The address to compare to target
+	//!
+	//! @return	The initial address pointed to by target
+	// ==============================================================================
+	OG_INLINE void *AtomicCompareExchangePointer( void **target, void *exchange, void *comparand ) {
+		void *old;
 #if OG_ASM_MSVC
 		__asm {
 			mov eax, comparand;
-			mov ecx, value;
+			mov ecx, exchange;
 			mov edx, target;
 			lock cmpxchg [edx], ecx;
+			mov old, eax;
 		}
 #elif OG_ASM_GNU(
 		__asm__ __volatile__(
 			"lock cmpxchg %3,%1"
-			: "=a" old, "=m" (*target)
-			:  "0" old, "r" comparand
+			: "=a" (old), "=m" (*(target))
+			:  "0" (comparand), "r" (exchange)
 		);
 #endif
 		return old;
 	}
 
-
-	// use for single producer and single consumer only
+	// ==============================================================================
+	//! Lock free queue for single producer single consumer scenarios
+	// ==============================================================================
 	template<typename type>
 	class LockFreeQueue {
 	private:
+		// ==============================================================================
+		//! A node
+		// ==============================================================================
 		struct Node {
+
+			// ==============================================================================
+			//! Constructor
+			//!
+			//! @param	val	The value
+			// ==============================================================================
 			Node( type *val ) : value(val), next(NULL) { }
-			Node() : next(NULL) {}
-			type *value;
-			Node* next;
+
+			type *	value;	//!< The value ( NULL for the divider )
+			Node *	next;	//!< The next node
 		};
-		Node* first;
-		Node *divider, *last;
+		Node *	first;		//!< The first node
+		Node *	divider;	//!< The divider node
+		Node *	last;		//!< The last node
 
 	public:
-		LockFreeQueue() { first = divider = last = new Node; }
+		// ==============================================================================
+		//! Default constructor ( create divider node and set first/last to the divider )
+		// ==============================================================================
+		LockFreeQueue() { first = divider = last = new Node(NULL); }
+
+		// ==============================================================================
+		//! Destructor ( delete all nodes )
+		// ==============================================================================
 		~LockFreeQueue() {
 			while( first != NULL ) {
 				Node* tmp = first;
@@ -93,8 +136,13 @@ namespace og {
 			}
 		}
 
-		void Produce( type *t ) {
-			last->next = new Node(t);
+		// ==============================================================================
+		//! Add an entry to the queue
+		//!
+		//! @param	value	A pointer to the object
+		// ==============================================================================
+		void Produce( type *value ) {
+			last->next = new Node(value);
 			AtomicExchangePointer( (void **)&last, last->next );
 
 			// remove consumed
@@ -104,6 +152,12 @@ namespace og {
 				delete tmp;
 			}
 		}
+
+		// ==============================================================================
+		//! Get the next entry in the queue and remove it from the queue
+		//!
+		//! @return	NULL if the queue is empty, otherwise the next entry
+		// ==============================================================================
 		type *Consume( void ) {
 			void *p = divider;
 			AtomicCompareExchangePointer( (void **)&p, NULL, last );
@@ -117,25 +171,35 @@ namespace og {
 		}
 	};
 
-	// multiple producer + multiple consumer
+	// ==============================================================================
+	//! Low lock queue for multi producer multi consumer scenarios
+	// ==============================================================================
 	template<typename type>
 	class LowLockQueue {
 	private:
-		LockFreeQueue<type> queue;
-		ogst::mutex	producerLock;
-		ogst::mutex	consumerLock;
+		LockFreeQueue<type>	queue;			//!< The queue
+		ogst::mutex			producerLock;	//!< The producer lock
+		ogst::mutex			consumerLock;	//!< The consumer lock
 
 	public:
+		// ==============================================================================
+		//! @copydoc LockFreeQueue::Produce
+		// ==============================================================================
 		void Produce( type *t ) {
 			producerLock.lock();
 			queue.Produce( t );
 			producerLock.unlock();
 		}
+
+		// ==============================================================================
+		//! @copydoc LockFreeQueue::Consume
+		// ==============================================================================
 		type *Consume( void ) {
 			ogst::unique_lock<ogst::mutex> lock(consumerLock);
 			return queue.Consume();
 		}
 	};
+	//! @}
 }
 
 #endif
