3cd9f97b7c45177018853260a127f2ec0fb4bfe5
Roughael <core@roughael.net>
2010-10-30 18:59:49 +0000

    added an idea for a job manager (untested)

---
+++ Libraries/Include/og/Common/JobManager.h
@@ -0,0 +1,207 @@
+// ==============================================================================
+//! @file
+//! @brief	Job Manager
+//! @author	Santo Pfingsten (TTK-Bandit)
+//! @note	Copyright (C) 2007-2010 Lusito Software
+// ==============================================================================
+//
+// The Open Game Libraries.
+//
+// This software is provided 'as-is', without any express or implied
+// warranty. In no event will the authors be held liable for any damages
+// arising from the use of this software.
+//
+// Permission is granted to anyone to use this software for any purpose,
+// including commercial applications, and to alter it and redistribute it
+// freely, subject to the following restrictions:
+//
+// 1. The origin of this software must not be misrepresented; you must not
+//    claim that you wrote the original software. If you use this software
+//    in a product, an acknowledgment in the product documentation would be
+//    appreciated but is not required.
+//
+// 2. Altered source versions must be plainly marked as such, and must not be
+//    misrepresented as being the original software.
+//
+// 3. This notice may not be removed or altered from any source distribution.
+//
+// ==============================================================================
+
+#ifndef __OG_JOB_MANAGER_H__
+#define __OG_JOB_MANAGER_H__
+
+#include <og/Common/Common.h>
+#include <og/Shared/Thread/LockFreeQueue.h>
+
+//! Open Game Libraries
+namespace og {
+//! @defgroup Common Common (Library)
+//! @{
+	class JobManager;
+
+	// ==============================================================================
+	//! Return value of a job
+	// ==============================================================================
+	enum JobResult {
+		JOB_DONE,	//!< Nothing happens ( user manually cleans the job )
+		JOB_REPEAT,	//!< Job gets re-added to the end of the job-queue
+		JOB_DELETE	//!< The job object will be deleted
+	};
+
+	// ==============================================================================
+	//! A job to execute
+	// ==============================================================================
+	class Job {
+	public:
+		// ==============================================================================
+		//! Virtual destructor
+		// ==============================================================================
+		virtual ~Job() {}
+
+		// ==============================================================================
+		//! Executes the job
+		//!
+		//! @return	JobResult: What to do next
+		//! 
+		//! @see JobResult
+		// ==============================================================================
+		virtual JobResult	Execute( void ) = 0;
+
+		// ==============================================================================
+		//! Called when a job is canceled due to the job manager stopping.
+		// ==============================================================================
+		virtual void		Cancel( void ) = 0;
+	};
+
+	// ==============================================================================
+	//! A worker thread
+	// ==============================================================================
+	class WorkerThread : public Thread {
+	public:
+		// ==============================================================================
+		//! Constructor
+		//!
+		//! @param	_manager	The manager of this worker
+		// ==============================================================================
+		WorkerThread( JobManager *_manager ) : job(NULL), manager(_manager) {}
+
+		// ==============================================================================
+		//! Starts the job
+		//!
+		//! @param	_job	The job
+		// ==============================================================================
+		void RunJob( Job *_job ) { job = _job; WakeUp(); }
+
+	private:
+		Job *		job;		//!< The job
+		JobManager *manager;	//!< The manager
+
+	protected:
+		// ==============================================================================
+		//! The thread main function
+		// ==============================================================================
+		void Run( void );
+	};
+
+	// ==============================================================================
+	//! A job manager
+	// ==============================================================================
+	class JobManager {
+	public:
+		// ==============================================================================
+		//! Default constructor
+		// ==============================================================================
+		JobManager() : numThreadsWanted(0), waitForDone(false) {}
+
+		// ==============================================================================
+		//! Destructor
+		// ==============================================================================
+		~JobManager();
+
+		// ==============================================================================
+		//! Add a job to the queue
+		//!
+		//! @param	job	The job to add
+		// ==============================================================================
+		void	AddJob( Job *job );
+
+		// ==============================================================================
+		//! Change the number workers
+		//!
+		//! @param	num	Number of workers
+		// ==============================================================================
+		void	SetNumWorkers( int num );
+
+		// ==============================================================================
+		//! Wait for all jobs to be done
+		// ==============================================================================
+		void	WaitForDone( void );
+
+	private:
+		friend class WorkerThread;
+
+		// ==============================================================================
+		//! Trigger the next job
+		// ==============================================================================
+		void	TriggerNextJob( void );
+
+		// ==============================================================================
+		//! A worker is done
+		//!
+		//! @param	worker	The worker
+		// ==============================================================================
+		void	WorkerIsDone( WorkerThread *worker );
+
+		// ==============================================================================
+		//! Add a free worker
+		//!
+		//! @param	worker	The worker. 
+		// ==============================================================================
+		void	AddFreeWorker( WorkerThread *worker );
+
+		ogst::mutex					listMutex;			//!< Protects numThreadsWanted and allThreads
+		int							numThreadsWanted;	//!< The number of wanted threads wanted
+		List<WorkerThread *>		allThreads;			//!< A list of all worker threads
+		bool						waitForDone;		//!< Waiting to be done ( see WaitForDone )
+		Condition					doneWaiter;			//!< The done waiter condition
+		LowLockQueue<WorkerThread>	availableThreads;	//!< The available (job-less) threads
+		LowLockQueue<Job>			jobList;			//!< The list of jobs to do
+
+		// ==============================================================================
+		//! The job that ends the WaitForDone()
+		// ==============================================================================
+		class JobStopWaitDone : public Job {
+		public:
+			// ==============================================================================
+			//! Constructor
+			//!
+			//! @param	mgr	The manager
+			// ==============================================================================
+			JobStopWaitDone( JobManager *mgr ) : manager(mgr) {}
+
+			// ==============================================================================
+			//! Stop waiting for done and signal the waiter
+			//!
+			//! @return	JOB_DELETE
+			// ==============================================================================
+			JobResult	Execute( void ) {
+				manager->waitForDone = false;
+				manager->doneWaiter.Signal();
+				return JOB_DELETE;
+			}
+
+			// ==============================================================================
+			//! Cancel should never be called on this one
+			// ==============================================================================
+			void		Cancel( void ) {
+				OG_ASSERT( false );
+			}
+
+		private:
+			JobManager *manager;	//!< The manager
+		};
+	};
+//! @}
+}
+
+#endif
---
+++ Libraries/Source/og/Common/JobManager.cpp
@@ -0,0 +1,191 @@
+/*
+===========================================================================
+The Open Game Libraries.
+Copyright (C) 2007-2010 Lusito Software
+
+Author:  Santo Pfingsten (TTK-Bandit)
+Purpose: Job Manager
+-----------------------------------------
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any source distribution.
+===========================================================================
+*/
+
+#include <og/Common/JobManager.h>
+
+namespace og {
+/*
+==============================================================================
+
+  WorkerThread
+
+==============================================================================
+*/
+
+/*
+================
+WorkerThread::Run
+================
+*/
+void WorkerThread::Run( void ) {
+	wakeUpEvent.Lock();
+	while( keepRunning ) {
+		wakeUpEvent.Wait();
+		if ( job ) {
+			switch( job->Execute() ) {
+				case JOB_DONE: break;
+				case JOB_REPEAT:
+					manager->AddJob( job );
+					break;
+				case JOB_DELETE:
+					delete job;
+					break;
+			}
+			job = NULL;
+		}
+		manager->WorkerIsDone( this );
+	}
+	wakeUpEvent.Unlock();
+}
+
+
+/*
+==============================================================================
+
+  JobManager
+
+==============================================================================
+*/
+
+/*
+================
+JobManager::JobManager
+================
+*/
+JobManager::~JobManager() {
+	WaitForDone();
+	listMutex.lock();
+	int num = allThreads.Num();
+	for( int i=0; i<num; i++ )
+		allThreads[i]->Stop(true);
+	allThreads.Clear();
+	listMutex.unlock();
+}
+
+/*
+================
+JobManager::AddJob
+================
+*/
+void JobManager::AddJob( Job *job ) {
+	if ( waitForDone )
+		job->Cancel();
+	else {
+		jobList.Produce( job );
+		TriggerNextJob();
+	}
+}
+
+/*
+================
+JobManager::SetNumWorkers
+================
+*/
+void JobManager::SetNumWorkers( int num ) {
+	listMutex.lock();
+	if ( num != numThreadsWanted ) {
+		numThreadsWanted = num;
+		while( numThreadsWanted < allThreads.Num() ) {
+			WorkerThread *worker = new WorkerThread(this);
+			allThreads.Append( worker );
+			AddFreeWorker( worker );
+		}
+	}
+	listMutex.unlock();
+}
+
+/*
+================
+JobManager::WaitForDone
+================
+*/
+void JobManager::WaitForDone( void ) {
+	listMutex.lock();
+	if ( allThreads.IsEmpty() )
+		return;
+	listMutex.unlock();
+
+	// do not allow adding new jobs while waiting for done
+	waitForDone = true;
+	doneWaiter.Lock();
+
+	// create a job and wait for it to be done
+	jobList.Produce( new JobStopWaitDone(this) );
+	TriggerNextJob();
+
+	while( waitForDone )
+		doneWaiter.Wait();
+	doneWaiter.Unlock();
+}
+
+/*
+================
+JobManager::TriggerNextJob
+================
+*/
+void JobManager::TriggerNextJob( void ) {
+	WorkerThread *worker = availableThreads.Consume();
+	if ( worker ) {
+		Job *job = jobList.Consume();
+		if( job != NULL )
+			worker->RunJob( job );
+		else
+			availableThreads.Produce( worker );
+	}
+}
+
+/*
+================
+JobManager::WorkerIsDone
+================
+*/
+void JobManager::WorkerIsDone( WorkerThread *worker ) {
+	listMutex.lock();
+	if ( numThreadsWanted >= allThreads.Num() )
+		AddFreeWorker( worker );
+	else {
+		worker->Stop( false );
+		int index = allThreads.Find( worker );
+		if ( index )
+			allThreads.Remove( index );
+	}
+	listMutex.unlock();
+}
+
+/*
+================
+JobManager::AddFreeWorker
+================
+*/
+void JobManager::AddFreeWorker( WorkerThread *worker ) {
+	availableThreads.Produce( worker );
+	TriggerNextJob();
+}
+
+}
